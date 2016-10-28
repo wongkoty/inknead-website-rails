1c66f6b6e57e6b8b6fbe23f8ab0465c1eca4f5c9
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-10-28 10:31:36 -0400

    making landing page

---
+++ inknead_website/app/assets/javascripts/components/home.jsx
@@ -0,0 +1,12 @@
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div className="home">
+				<div className="headline">
+					<h1>BEST HEADLINE</h1><br />
+					<h1>IN THE WORLD</h1>
+				</div>
+			</div>
+		)
+	}
+})
--- inknead_website/app/assets/javascripts/components/test.js.jsx
+++
@@ -1,9 +0,0 @@
-var Test = React.createClass({
-	render: function() {
-		return (
-			<div>
-				check me out
-			</div>
-			)
-	}
-})
