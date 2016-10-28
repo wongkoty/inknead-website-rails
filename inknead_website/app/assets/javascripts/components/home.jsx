e2e4fede6e5ce0586e41ba942189c60a6e090371
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-10-28 15:57:35 -0400

    adding logo and iphone

--- inknead_website/app/assets/javascripts/components/home.jsx
+++ inknead_website/app/assets/javascripts/components/home.jsx
@@ -1,10 +1,24 @@
 var Home = React.createClass({
+
 	render: function() {
+
 		return (
 			<div className="home">
-				<div className="headline">
-					<h1>BEST HEADLINE</h1><br />
-					<h1>IN THE WORLD</h1>
+			<div className='logo-container'>
+				<img src='assets/logo@2x.png' className='logo'/>
+			</div>
+				<div className="main-text">
+					<div className="headline">
+						<p>BEST HEADLINE IN THE WORLD</p>
+					</div>
+
+					<div className="headline-info-temp">
+						<p>Lorem ipsum dolor sit amet, consectetua adipiscing elit. 
+						Verstibulum rutrum urna felis.</p>
+					</div>
+				</div>
+				<div className="iphone-container">
+					<img src='assets/iphone_big.png' className='iphone'/>
 				</div>
 			</div>
 		)
