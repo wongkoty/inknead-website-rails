f528e0a7affa700c25172ba77b9861e2e28309a7
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-10-31 14:34:50 -0400

    finsihing first component

--- inknead_website/app/assets/javascripts/components/home.jsx
+++ inknead_website/app/assets/javascripts/components/home.jsx
@@ -5,35 +5,42 @@ var Home = React.createClass({
 		return (
 			<div className="container-fluid">
 				<div className="home">
-				<div className='logo-container'>
-					<img src='assets/logo@2x.png' className='logo'/>
-				</div>
+
+					<div className='logo-container'>
+						<img src='assets/logo@2x.png' className='logo'/>
+					</div>
+					<div className='social-container'>
+						<img src='assets/icon_facebook@2x.png' className='social-icons'/>
+						<img src='assets/icon_twitter@2x.png' className='social-icons'/>
+						<img src='assets/share@2x.png' className='social-icons share-icon'/>
+					</div>
+
 					<div className="main-text">
 						<div className="headline">
 							<p>BEST HEADLINE IN THE WORLD</p>
 						</div>
-
 						<div className="headline-info-temp">
 							<p>Lorem ipsum dolor sit amet, consectetua adipiscing elit. 
 							Verstibulum rutrum urna felis.</p>
 						</div>
-
 						<div className='email-form-container'>
 							<form>
 								<input type='text' name='email' placeholder='Your E-mail' className='email-input'/>
-								<input type='submit' className='email-submit' />
+								<input type='submit' className='white-button' />
 							</form>
 						</div>
+						<div className='learn-demo-container'>
+							<button className='white-button learn-button'>Learn More</button>
+							<button className='demo-button'>Demo</button>
+
+						</div>
 					</div>
+					
 
-
-
-					<div className="iphone-container img-responsive">
-						<img src='assets/iphone_big.png' className='iphone'/>
+					<div className="iphone-container">
+						<img src='assets/iphone_big.png' className='iphone img-responsive'/>
 					</div>
 				</div>
-
-				<button type="button" className="btn btn-info">Info</button>
 			</div>
 		)
 	}
