eeeca5c08dd84c3145edc39b1864b887febf5e42
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-11-02 17:46:25 -0400

    adding mailchimp

--- inknead_website/app/controllers/landing_page_controller.rb
+++ inknead_website/app/controllers/landing_page_controller.rb
@@ -6,6 +6,12 @@ class LandingPageController < ApplicationController
 
   def new
   	puts 'in the new'
+  	puts params[:email]
+  	# puts ENV["MAILCHIMP_LIST_ID"]
+  	gb = Gibbon::API.new
+    gb.lists.subscribe({:id => ENV["MAILCHIMP_LIST_ID"], 
+    										:email => {:email => params[:email]}, :double_optin => false}) 
+  	redirect_to root_path
   end
 
 end
